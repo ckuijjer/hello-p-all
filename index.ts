ff7742c85bcfb218d0282d561a922e5fa2aee385
Casper Kuijjer <casper@kuijjer.com>
2022-06-28 11:50:28 +0200

    p-all v4

---
+++ index.ts
@@ -0,0 +1,15 @@
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const fn = (msg) => async () => {
+  console.log(`starting with ${msg}`);
+  await sleep(1000);
+  console.log(`done with ${msg}`);
+  return msg;
+};
+
+(async () => {
+  const pAll = await import('p-all');
+
+  const actions = [fn(1), fn(2), fn(3), fn(4)];
+  console.log(await pAll.default(actions, { concurrency: 2 }));
+})();
